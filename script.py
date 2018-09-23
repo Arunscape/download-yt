954dcfddb3b9d57cdb78400735ca5340612e6770
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-09-22 18:24:41 -0600

    added channels

--- script.py
+++ script.py
@@ -4,6 +4,38 @@ import feedparser
 # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 #     ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
 
+techChannels = [
+'https://www.youtube.com/user/LinusTechTips',
+'https://www.youtube.com/user/Jayztwocents',
+'https://www.youtube.com/channel/UCeeFfhMcJa1kjtfZAGskOCA',
+'https://www.youtube.com/user/Techquickie',
+'https://www.youtube.com/user/ChannelSuperFun',
+'https://www.youtube.com/user/MemoryExpress'
+]
+
+hermitCraft = [
+'https://www.youtube.com/user/ThatMumboJumbo',
+'https://www.youtube.com/user/rendog',
+'https://www.youtube.com/user/ZedaphPlays',
+'https://www.youtube.com/user/AhlViktor'
+]
+
+educational = [
+'https://www.youtube.com/user/msadaghd',
+'https://www.youtube.com/channel/UCKKiQ8Bz3pgiJ7JdqNrQfeQ',
+'https://www.youtube.com/channel/UCu6mSoMNzHQiBIOCkHUa2Aw',
+'https://www.youtube.com/channel/UC2MJylovjrLtsGP0_4UrqrQ',
+'https://www.youtube.com/user/physicswoman',
+'https://www.youtube.com/channel/UCSIvk78tK2TiviLQn4fSHaw',
+'https://www.youtube.com/channel/UC0e3QhIYukixgh5VVpKHH9Q',
+'https://www.youtube.com/user/minutephysics',
+'https://www.youtube.com/channel/UC3KEoMzNz8eYnwBC34RaKCQ'
+]
+
+other = [
+'https://www.youtube.com/user/CBC22minutes/videos'
+]
+
 
 if __name__ == "__main__":
     print("Hello")
